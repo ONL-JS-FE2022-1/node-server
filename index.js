7d9bf77cb41a4fd1a362ac8e8f984605946e4143
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-01-12 20:04:10 +0200

    Server send HTML

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const http = require('http');
+const fs = require('fs/promises'); 
 
 const PORT = 3000;
 
 const requestListener = (req, res) => {
-    // console.log('REQUEST IS HERE')
-    // const {url, method} = req;
-    // console.log(url, method);
-    res.statusCode = 404;
-    res.end('HELLO FROM SERVER');
+    const {url} = req;
+
+    if( url === '/index.html' ) {
+        fs.readFile('./views/index.html', 'utf8')
+        .then((data) => {
+            res.end(data)
+        })
+    } else {
+        res.statusCode = 404;
+        fs.readFile('./views/error.html', 'utf8')
+        .then((data) => {
+            res.end(data)
+        })
+    }
 }
 
 const server = http.createServer(requestListener);
