57ce19feee4c7b8bd3593f17aa5712b1350c4ab8
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-01-12 19:48:27 +0200

    Server response

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const PORT = 3000;
 
 const requestListener = (req, res) => {
     // console.log('REQUEST IS HERE')
-    const {url, method} = req;
-    console.log(url, method);
+    // const {url, method} = req;
+    // console.log(url, method);
+    res.statusCode = 404;
+    res.end('HELLO FROM SERVER');
 }
 
 const server = http.createServer(requestListener);
