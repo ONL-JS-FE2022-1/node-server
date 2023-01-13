f4f42147fb56ad6db9aa1bef01d3e73607e70f9b
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-01-13 20:49:46 +0200

    Model + Controller + refactor

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
+const {validateUser} = require('./middlewares/index');
+const UserController = require('./controllers/UserController');
+
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, world');
-});
+const bodyParser = express.json();
+
+app.post('/user', bodyParser, validateUser, UserController.createUser);
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
+
+/*
+Зареєструвати юзера
+
+1. Прийняти запит на певний endpoint
+2. Розпасрити дані, які прийшли з запитом.
+3. Перевірити (валідувати) дані.
+4. Зберегти ці дані.
+5. Створити сесію юзера.
+6. Підготувати дані юзера у відповідь.
+7. Надіслати підготовлені дані у відповідь на запит.
+
+*/
