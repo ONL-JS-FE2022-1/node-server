cfd62e3a01ce6f848f5d0d05d72b49a39fd6843d
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-01-16 18:04:31 +0200

    fixed port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const {validateUser} = require('./middlewares/index');
 const UserController = require('./controllers/UserController');
 
-const PORT = 3000;
+const PORT = 5000;
 
 const bodyParser = express.json();
 
